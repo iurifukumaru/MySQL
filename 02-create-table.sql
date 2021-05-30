1ac63810cbb70ecdf251f9585749e9adda5c65ba
Iuri Fukumaru <66225493+iurifukumaru@users.noreply.github.com>
2021-05-30 17:36:16 -0400

    Update 02-create-table.sql

--- 02-create-table.sql
+++ 02-create-table.sql
@@ -11,3 +11,10 @@ SELECT COUNT(*) FROM test;
 SELECT * FROM test;
 SELECT table_name, engine FROM information_schema.tables WHERE table_schema = 'scratch';
 DROP TABLE IF EXISTS test;
+
+-- selecting rows
+use world;
+select 'Hello, World';
+select * from Country order by Name;
+select count(*) from Country;
+select * from Country order by Name LIMIT 50,5;
